f3749ff5319da3ba33fd2bbbcff339d2d9e1a971
Omar28Radiance <omarin0a@hotmail.com>
2022-05-26 13:00:08 -0500

    Creación de componentes para la carpeta "utils"

---
+++ src/utils/FirebaseUtil.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+function config() {
+    firebase.initializeApp({
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: ""
+    });
+}
