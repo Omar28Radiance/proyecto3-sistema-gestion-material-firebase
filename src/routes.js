5c52fe8fe623050b668549ae786f28ceda758823
Omar28Radiance <omarin0a@hotmail.com>
2022-06-08 12:25:49 -0500

    Corrección de 'Resgister.js' to 'Register.js'

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import { Navigate } from 'react-router-dom';
+import DashboardLayout from './components/DashboardLayout';
+import MainLayout from './components/MainLayout';
+import Account from './pages/Account';
+import CustomerList from './pages/CustomerList';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import ProductList from './pages/ProductList';
+import Register from './pages/Register'
+import
+
+const routes = []
